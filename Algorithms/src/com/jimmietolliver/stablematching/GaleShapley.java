c6cc85a24cc367375d6ca75736938409c7c74db6
JimmieTolliver <jtolliver@mac.com>
2020-03-23 19:14:34 -0400

    add Karatsuba algorithm

---
+++ Algorithms/src/com/jimmietolliver/stablematching/GaleShapley.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jimmietolliver.stablematching;
+
+/**
+ * @author Jimmie Tolliver
+ *
+ */
+public class GaleShapley {
+
+	/**
+	 * 
+	 */
+	public GaleShapley() {
+	
+		// TODO
+	}
+
+}
---
+++ Algorithms/src/com/jimmietolliver/test/KaratsubaTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.jimmietolliver.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import com.jimmietolliver.divideandconquer.Karatsuba;
+
+/**
+ * @author Jimmie Tolliver
+ *
+ */
+public class KaratsubaTest {
+
+	@Test
+	public void test() {
+		BigInteger x = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
+		BigInteger y = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
+		
+		Karatsuba k = new Karatsuba();
+		
+		BigInteger result = k.karatsuba(x, y);
+		
+		System.out.println(result);
+		
+		assertEquals(y.multiply(x), result);
+	}
+
+}
