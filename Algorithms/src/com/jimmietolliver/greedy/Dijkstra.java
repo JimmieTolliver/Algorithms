be68f8ffc9571d22e27a4f49fd02ab2d47c7c306
JimmieTolliver <jtolliver@mac.com>
2020-03-24 13:24:48 -0400

    add Dijkstra pseudocode

---
+++ Algorithms/src/com/jimmietolliver/greedy/Dijkstra.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.jimmietolliver.greedy;
+
+/**
+ * @author Jimmie Tolliver
+ *
+ */
+public class Dijkstra {
+
+//  PseudoCode	
+//	Input: directed graph G = (V,E) in adjacency-list
+//			representation, a vertex element of V , a length le >= 0 for
+//			each e element of E.
+//	Postcondition: for every vertex v, the value len(v)
+//			equals the true shortest-path distance dist(s,v).
+	
+	
+//			// Initialization
+//			X := {s}
+//			len(s) := 0, len(v) := +1 for every v 6= s
+//			// Main loop
+//			while there is an edge (v,w) with v element of X and w is not element of X do
+//			 	(v*,w* ) := such an edge minimizing len(v) + lvw
+//			 	add w* to X
+//			 	len(w* ) := len(v*) + lv*w*
+
+	/**
+	 * 
+	 */
+	public Dijkstra() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
